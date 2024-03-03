142f2a51f2fe11c5958cb02c48116895e69a1110
dekkusudev <132282587+dekkusudev@users.noreply.github.com>
2024-03-04 02:14:56 +0400

    Update offchat.py

--- offchat.py
+++ offchat.py
@@ -1,49 +1,47 @@
-from pyrogram import Client, filters
+from hikkatl.types import Message
+from pyrogram import filters
 from pyrogram.types import ChatPermissions
+from .. import loader, utils
 
-@Client.on_message(filters.command("offchat") & filters.me)
-async def restrict_chat(client, message):
-    if message.chat.type not in ["supergroup", "channel"]:
-        await message.reply("Я могу ограничивать только супергруппы и каналы")
-        return
+@loader.tds
+class OffChat(loader.Module):
+    """OffChat"""
+    strings = {"name": "OffChat",
+               "not_supergroup": "Я могу ограничивать только супергруппы и каналы",
+               "not_admin": "Вы должны быть администратором, чтобы ограничить чат",
+               "chat_closed_night": "Чат {} закрыт на ночь. Писать могут только администраторы",
+               "chat_closed": "Чат {} закрыт. Писать могут только администраторы"}
 
-    # Получить информацию о чате
-    chat_id = message.chat.id
-    chat = await client.get_chat(chat_id)
+    @loader.command(pattern="offchat")
+    async def offchat(self, message: Message):
+        """Close chat at night"""
+        if message.chat.type not in ["supergroup", "channel"]:
+            await utils.answer(message, self.strings("not_supergroup"))
+            return
+        chat_id = message.chat.id
+        chat = await client.get_chat(chat_id)
+        admins = await client.get_chat_members(chat_id, filter="administrators")
+        user_id = message.from_user.id
+        user_is_admin = any(admin.user.id == user_id for admin in admins)
+        if not user_is_admin:
+            await utils.answer(message, self.strings("not_admin"))
+            return
+        await client.set_chat_permissions(chat_id, ChatPermissions())
+        await utils.answer(message, self.strings().format("chat_closed_night",chat.title))
 
-    # Проверить, является ли пользователь администратором
-    admins = await client.get_chat_members(chat_id, filter="administrators")
-    user_id = message.from_user.id
-    user_is_admin = any(admin.user.id == user_id for admin in admins)
-
-    if not user_is_admin:
-        await message.reply("Вы должны быть администратором, чтобы ограничить чат")
-        return
-
-    # Установка разрешений на чат
-    await client.set_chat_permissions(chat_id, ChatPermissions())
-    await message.reply(f"Чат {chat.title} закрыт на ночь. Писать могут только администраторы")
-
-@Client.on_message(filters.command("soffchat") & filters.me)
-async def restrict_chat(client, message):
-    if message.chat.type not in ["supergroup", "channel"]:
-        await message.reply("Я могу ограничивать только супергруппы и каналы")
-        return
-
-    # Получить информацию о чате
-    chat_id = message.chat.id
-    chat = await client.get_chat(chat_id)
-
-    # Проверить, является ли пользователь администратором
-    admins = await client.get_chat_members(chat_id, filter="administrators")
-    user_id = message.from_user.id
-    user_is_admin = any(admin.user.id == user_id for admin in admins)
-
-    if not user_is_admin:
-        await message.reply("Вы должны быть администратором, чтобы ограничить чат")
-        return
-
-    # Установка разрешений на чат
-    await client.set_chat_permissions(chat_id, ChatPermissions())
-    await message.reply(f"Чат {chat.title} закрыт. Писать могут только администраторы")
-
+    @loader.command(pattern="soffchat")
+    async def soffchat(self, message: Message):
+        """Close chat"""
+        if message.chat.type not in ["supergroup", "channel"]:
+            await utils.answer(message, self.strings("not_supergroup"))
+            return
+        chat_id = message.chat.id
+        chat = await client.get_chat(chat_id)
+        admins = await client.get_chat_members(chat_id, filter="administrators")
+        user_id = message.from_user.id
+        user_is_admin = any(admin.user.id == user_id for admin in admins)
+        if not user_is_admin:
+            await utils.answer(message, self.strings("not_admin"))
+            return
+        await client.set_chat_permissions(chat_id, ChatPermissions())
+        await utils.answer(message, self.strings().format("chat_closed",chat.title))
