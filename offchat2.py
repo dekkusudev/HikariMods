b90c711fda67f3f25d03a1f3e66552c256e27c88
dekkusudev <132282587+dekkusudev@users.noreply.github.com>
2024-03-04 02:22:16 +0400

    Update offchat2.py

--- offchat2.py
+++ offchat2.py
@@ -16,26 +16,26 @@ class OffChat(loader.Module):
     async def offchat(self, message: Message):
         """Close chat at night"""
         chat_id = message.chat.id
-        chat = await client.get_chat(chat_id)
-        admins = await client.get_chat_members(chat_id, filter="administrators")
+        chat = await message.client.get_chat(chat_id)
+        admins = await message.client.get_chat_members(chat_id, filter="administrators")
         user_id = message.from_user.id
         user_is_admin = any(admin.user.id == user_id for admin in admins)
         if not user_is_admin:
             await utils.answer(message, self.strings("not_admin"))
             return
-        await client.set_chat_permissions(chat_id, ChatPermissions())
+        await message.client.set_chat_permissions(chat_id, ChatPermissions())
         await utils.answer(message, self.strings().format("chat_closed_night",chat.title))
 
     @loader.command(pattern="soffchat")
     async def soffchat(self, message: Message):
         """Close chat"""
         chat_id = message.chat.id
-        chat = await client.get_chat(chat_id)
-        admins = await client.get_chat_members(chat_id, filter="administrators")
+        chat = await message.client.get_chat(chat_id)
+        admins = await message.client.get_chat_members(chat_id, filter="administrators")
         user_id = message.from_user.id
         user_is_admin = any(admin.user.id == user_id for admin in admins)
         if not user_is_admin:
             await utils.answer(message, self.strings("not_admin"))
             return
-        await client.set_chat_permissions(chat_id, ChatPermissions())
+        await message.client.set_chat_permissions(chat_id, ChatPermissions())
         await utils.answer(message, self.strings().format("chat_closed",chat.title))
